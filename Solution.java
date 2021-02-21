aa77aafc65fb932d806852e48ad67d68fdacd33c
Cesar Ochoa Navarro <76407383+CesarOchoaNavarro@users.noreply.github.com>
2021-02-21 15:05:12 -0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,33 +1,4 @@
 public class Solution {
-    /*
-    public boolean checkIfExist(int[] arr) {
-        int len = arr.length;
-        //int f = 0;
-        for(int i = 0; i < len; i++) {
-
-            //if negative do the original way
-
-            //divide the first element by two and check if that is in the list
-            if(arr[i] % 2 == 0) {
-                int t = arr[i] / 2;
-
-                for(int j = 0; j < len; j++) {
-                    if(t == arr[j])
-                    {
-                        return true;
-                    }
-                }
-            }
-
-        }
-
-        //if(f == 1)
-        //return true;
-
-        return false;
-
-    }
-     */
     public void rotate(int[] nums, int k) {
         int len = nums.length;
 
